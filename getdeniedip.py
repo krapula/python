974eaa44a6e49e70e4f4f90ebd02048ea532ff0b
krapula <krapula@users.noreply.github.com>
2014-12-25 21:53:49 +0100

    change to work with list split instead of string slices

--- getdeniedip.py
+++ getdeniedip.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
 """
 Reads Denyhosts denied IP list and returns details of each IP.
-Library 'requests' is not standard. "pip install requests" if necessary.
 """
 import time
 import requests
 from os.path import expanduser
 
 def cmonth(month):
-    # Returns the monthnumber for the three letter representation
     months = {
             'Jan':'01',
             'Feb':'02',
@@ -25,14 +23,42 @@ def cmonth(month):
             }
     return months[month]
 
+def cdaymonth(d):
+    if len(d) == 1:
+        d = '0' + d
+
+    return d
+
 def checkip(ip):
     # returns csv formatted line of textinfo about IP as specified in http://ip-api.com/docs/api:csv
     site = "http://ip-api.com/csv/"
     r = requests.get(site+ip)
     return r.text
 
+def getLineInfo(line):
+    info = {
+            'weekday':'',
+            'month':'',
+            'monthno':'',
+            'daymonth':'',
+            'time':'',
+            'year':'',
+            'ip':''
+            }
 
-# Starting program. Skip first 20 lines (junk) and then continue.
+    s = line.split()
+    info['weekday']     = s[2]
+    info['month']       = s[3]
+    info['monthno']     = cmonth(s[3])
+    info['daymonth']    = cdaymonth(s[4])
+    info['time']        = s[5]
+    info['year']        = s[6]
+    info['ip']          = s[9]
+
+    return info
+
+
+# Starting program
 lineno = 1
 home = expanduser("~")
 destfile = open(home+'/result_log.csv', 'w')
@@ -42,26 +68,20 @@ for i in xrange(20):
     source.next()
 
 for line in source:
-    if line[:1] == '#'
+    if line[:1] == '#':
         if lineno % 250 == 0:
             print 'Pausing for a minute. Max 250 lookups per minute.'
             time.sleep(60)
 
-        weekday     = line[13:16]
-        month       = line[17:20]
-        monthno     = cmonth(month)
-        daymonth    = line[21:23].replace(' ','0')
-        time        = line[24:32]
-        year        = line[33:37]
-        ip          = line[46:].rstrip('\r\n')
+        info = getLineInfo(line)    
 
-        print '{}: IP {}'.format(lineno,ip)
-        lineinfo = '{},{}-{}-{},{},{},'.format(weekday,year,monthno,daymonth,time,ip)
-        result = checkip(ip)
+        print '{}: IP {}'.format(lineno, info['ip'])
+        lineinfo = '{},{}-{}-{},{},{}'.format(info['weekday'], info['year'], info['monthno'], info['daymonth'], info['time'], info['ip'])
+        result = checkip(info['ip'])
 
         if result[:4] == 'fail':
             print 'Error code received. Aborting.'
             break
         else:
-            destfile.write(result + lineinfo + '\n')
+            destfile.write(result + ',' + lineinfo + '\n')
             lineno += 1
